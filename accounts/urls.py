68f852255beee52430009b04fe53f87937883e8e
hyun98 <hyun0404woo@gmail.com>
2021-04-06 14:05:48 +0900

    settings.py del

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),    
     path('signup/', views.signup, name='signup'),
-    path('/<str:username>/', views.people, name="people"),
+    path('<str:username>/', views.people, name="people"),
     path('profile_modify/<str:username>', views.profile_modify, name="profile_modify"),
     path('<str:username>/follow/', views.follow, name="follow"),
 ]
